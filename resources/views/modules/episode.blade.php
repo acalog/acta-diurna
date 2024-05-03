04f7d4c81351ecd1f0ba7f7b9f5da6f136337135
Alex Caloggero <a.calog@protonmail.com>
2024-05-03 17:02:42 -0400

    Don't load background image if not needed

--- resources/views/modules/episode.blade.php
+++ resources/views/modules/episode.blade.php
@@ -4,7 +4,7 @@
             <a href="{{ route('podcasts.edit', $podcast->id) }}" class="admin-item">Edit Podcast</a>
         </div>
     @endisAdmin
-    
+
     <div class="episode-item">
         <a href="{{ route('getPodcastBySlug', Str::lower(str_replace(' ', '_', $podcast->title))) }}">
             <h2>S{{ $podcast->season }} E{{ $podcast->episode }} - {{ $podcast->title }} </h2>
